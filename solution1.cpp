7299d13e2aa790c03769d3a51f997d03dcb66743
csh8696-git <csh8696@gmail.com>
2025-12-26 09:26:23 +0900

    solution2_15
    나머지가 1이 되는 수

---
+++ solution1.cpp
@@ -0,0 +1,42 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
+
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int sumDivisor(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= sqrt(n); i++) if (n % i == 0) { sum += i; if (n != i * i) sum += n / i; }
+    return sum;
+}
+
+int main()
+{
+    int testCase = 12;
+    int testAnswer = sumDivisor(testCase);
+
+    cout << testAnswer;
+}
+
+#include <cmath>
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= sqrt(n); i++)
+        answer += !(n % i) ? i == n / i ? i : i + n / i : 0;
+    return answer;
+}
---
+++ solution2.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 1) {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
+
+int main() {
+    solution(15);
+}
+
+int solution(int n) {
+    if (n & 1) return 2;
+    else for (int i = 3; i < n; i += 2) if (n % i == 1) return i;
+}
